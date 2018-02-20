afcd5c23b6e0ccbfe0aef8be9399d47b73098434
Octav-C <octavian.c96@gmail.com>
2018-02-20 09:50:12 +0200

    Added Main class

---
+++ APPOO_Lab1/src/main/Main.java
@@ -0,0 +1,25 @@
+package lab1;
+
+public class Main {
+
+	public static void main(String[] args) {		
+		System.out.println("Hi5");
+		Accounts[] acc  =  new Accounts [5] ;
+		acc[0] = new Accounts ();
+		acc[0].setAcc_number(1111111);
+	    System.out.println(acc[0].getAcc_number());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
